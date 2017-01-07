06cec00aed2e0627bab53d11b6841e3e5ac74cbf
Rob S <rob.sherling@gmail.com>
2017-01-07 18:41:50 +0900

    Clear up main file

--- main.rb
+++ main.rb
@@ -6,12 +6,6 @@ agent = Mechanize.new
 agent.user_agent_alias = "Mac Safari"
 page = agent.get("file:///#{Dir.pwd}/page.html")
 
-# 1 Div class videoUList
-# 1 ul id = moreData
-#  each li
-#  div class thumbnail-info-wrapper clearfix
-# span class title
-# in span: a href=URI title='TITLE'
 CSV.open('results.csv', 'w') do |csv|
 page.at('.videoUList').search('#moreData li').
   search('.thumbnail-info-wrapper').search('.title').each do |t|
